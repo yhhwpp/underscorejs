446fe5da9cc55d7815270963d8013ae555b44928
hehe <helpzp2008@126.com>
2017-04-07 18:38:46 +0800

    underscore 常用静态方法增加

--- underscore.js
+++ underscore.js
@@ -29,7 +29,7 @@
         }
         exports._ = _;
     } else {
-        root._ = this;
+        root._ = _;
     }
     _.VERSION = '1.8.3'; //版本号
     var optimizeCb = function (func, context, argCout) { //内部方法，根据context以及参数数量，返回一些回调，迭代方法
@@ -52,6 +52,74 @@
             return func.apply(context, arguments);
         }
     };
+    var cb = function (value, context, argCout) { //callback的统一处理，应用于collection的每个元素
+        if (value == null) return _.identity;
+        if (_.isFunction(value)) return optimizeCb(value, context, argCout);
+        if (_.isObject(value)) return _.matcher(value);
+        return _.property(value);
+    };
+    _.iteratee = function (value, context) {
+        return cb(value, context, Infinity);
+    };
+    var createAssigner = function (keysFunc, undefineOnly) { // 用到的函数 _.extend = createAssigner(_.allKeys); _.extendOwn  = createAssigner(_.keys); _.defaults = createAssigner(_.allKeys, true)
+        return function (obj) {
+            var length = arguments.length;
+            if (length < 2 || obj == null) return obj;
+            for (var index = 1; index < length; index++) { // 获取第一个参数除外的对象参数
+                var source = arguments[index],
+                    keys = keysFunc(source),
+                    l = keys.length;
+                for (var i = 0; i < l; i++) {
+                    var key = keys[i];
+                    if (!undefineOnly || obj[key] === void 0) obj[key] = source[key]; // _.defaults中有key 的取以首次出现的, 而_.extend 和 _.extendOwn后面对象的键值对直接覆盖前面的
+                }
+            }
+            return obj;
+        };
+    };
+    var baseCreate = function (prototype) { // 继承函数
+        if (!_.isObject(prototype)) return {};
+        if (nativeCreat) return nativeCreat(prototype);
+        Ctor.prototype = prototype;
+        var result = new Ctor;
+        Ctor.prototype = null; // 释放Ctor.prototype的内存，保证Ctor是真正的空函数，这里Ctor.prototype重新分配了地址，并不影响实例result的原型链
+        return result;
+    };
+    var property = function (key) {
+        return function (obj) {
+            return obj == null ? void 0 : obj[key];
+        }
+    };
+
+    var MAX_ARRAY_INDEX = Math.pow(2, 52) - 1;// JavaScript做大的数值
+    var getLength = property('length'); // 用来获取 array 以及 arrayLike 元素的 length 属性值
+    var isArrayLike = function (collection) {//包括数组、arguments、HTML Collection 以及 NodeList,字符串，函数，以及具有键{length}的对象
+        var length = getLength(collection);
+        return typeof length == 'number' && length >= 0 && length <= MAX_ARRAY_INDEX;
+    };
+
+
+
+    // 扩展方法
+    _.each = _.forEach = function (obj, iteratee, context) {
+        iteratee = optimizeCb(iteratee, context);
+        var i, length;
+        if (isArrayLike(obj)) {
+            for (i = 0, length = obj.length; i < length; i++) {
+                iteratee(obj[i], i, obj);
+            }
+        } else { // 如果是对象，遍历处理 values 值
+            var keys = _.keys(obj);
+            for (i = 0, length = obj.length; i < length; i++) {
+                iteratee(obj[keys[i]], keys[i], obj);
+            }
+        }
+        return obj;
+    };
+
+    _.property = property;
+
+
 
     if (typeof /./ != 'function' && typeof Int8Array != 'object') { // _.isFunction兼容处理
         _.isFunction = function (obj) {
@@ -59,7 +127,41 @@
         };
     }
 
-
+    _.isObject = function (obj) { // 判断是否为对象，这里对象包括function 和 object
+        var type = typeof obj;
+        return type === 'function' || type == 'object' && !!obj; // 这里排除null
+    };
+    _.matcher = _.matches = function (attrs) {// 判断一个给定的对象是否有某些键值对
+        attrs = _.extendOwn({}, attrs);
+        return function (obj) {
+            return _.isMatch(obj, attrs);
+        }
+    };
+    _.isMatch = function (object, attrs) { // 判断obj是否有attrs中的所有的key-value键值对
+        var keys = _keys(attrs), length = keys.length;
+        if (object == null) return !length;
+        var obj = Object(object); // 这一步感觉没必要
+        for (var i = 0; i < length; i++) {
+            var key = keys[i];
+            if (attrs[key] !== obj[key] || !(key in obj)) return false;
+        }
+        return true;
+    };
+    _.keys = function (obj) {
+        if (!_.isObject(obj)) return [];
+        if (nativeKeys) return nativeKeys(obj);
+        var keys = [];
+        for (var key in obj) if (_has(obj, key)) keys.push(key);
+        return keys;
+    };
+    _.has = function (obj, key) { //判断对象中是否有指定 key
+        return obj != null && hasOwnProperty.call(obj, key); // 这里是防止hasOwnProperty 作为属性带来问题, 参见https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Object/hasOwnProperty
+    };
+    _.identity = function (value) { // 返回传入的参数
+        return value
+    }
+    _.extendOwn = _.assign = createAssigner(_.keys); //跟 extend 方法类似，但是只把 own properties 拷贝给第一个参数对象  只继承 own properties 的键值对
+    // 参数个数 >= 1
 
 
 }.call(this)); // 设置匿名函数的context外层全局变量，浏览器环境为window.
