e861d114b8609ba99070ff54457d6bf4edc16a30
hehe <helpzp2008@126.com>
2017-04-19 17:36:49 +0800

    增加object方法

--- underscore.js
+++ underscore.js
@@ -438,6 +438,17 @@
             result[index] = _.pluck(array, index);
         }
         return result;
+    }; 
+    _.object = function (list, values) {
+        var result = {};
+        for (var i = 0, length = getLength(list); i < length; i++) {
+            if (values) {
+                result[list[i]] = values[i];
+            } else {
+                result[list[i][0]] = list[i][1];
+            }
+        }
+        return result;
     };
 
 
