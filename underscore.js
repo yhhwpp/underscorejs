a4a73706d981e5df36abd43be8dea51d1af2e654
hehe <helpzp2008@126.com>
2017-04-28 18:37:08 +0800

    增加

--- underscore.js
+++ underscore.js
@@ -554,20 +554,77 @@
         return bound;
     };
     _.partial = function (func) {
-        var boundArgs = slice(arguments, 1);
+        var boundArgs = slice.call(arguments, 1);
         var bound = function () {
             var position = 0,
                 length = boundArgs.length;
             var args = Array(length);
             for (var i = 0; i < length; i++) {
-                args[i] = boundArgs[i] === _ ? arguments[position++] : boundArgs[i];
+                args[i] = boundArgs[i] === _ ? arguments[position++] : boundArgs[i]; // 如果该位置的参数为 _，则用 bound 方法的参数填充这个位置  args 为调用 _.partial 方法的 pre-fill 的参数 & bound 方法的 arguments
             }
-            while (position < arguments.length) args.push(arguments[postion++]);
+            while (position < arguments.length) args.push(arguments[postion++]); // bound 方法还有剩余的 arguments，添上去
             return executeBound(func, bound, this, this, args)
         };
-        return bound
+        return bound;
     };
-
+    _.bindAll = function (obj) {
+        var i, length = arguments.length,
+            key;
+        if (arguments <= 1) throw new Error('bindAll must be passed function names');
+        for (i = 1; i < length; i++) {
+            key = arguments[i];
+            obj[key] = _.bind(obj[key], obj);
+        }
+        return obj;
+    };
+    _.memoize = function (func, hasher) {
+        var memoize = function (key) {
+            var cache = memoize.cache;
+            var address = '' + (hasher ? hasher.apply(this, arguments) : key);
+            if (!_has(cache, address)) cache[address] = func.apply(this, arguments);
+            return cache[address];
+        }
+        memoize.cache = {};
+        return memoize;
+    }
+    _.delay = function (func, wait) { //异步执行函数，执行开销大的计算和无阻塞UI线程的HTML渲染时候非常有用
+        var args = slice.call(arguments, 2);
+        return setTimeout(function () {
+            return func.apply(null, args);
+        }, wait);
+    };
+    _.defer = _.partial(_.delay, _, 1); // 延迟调用function， 类似于setTimeout 0
+    _.throttle = function (func, wait, options) {
+        var context, args, result;
+        var timeout = null;
+        var previous = 0;
+        if (!options) options = {};
+        var later = function () {
+            previous = options.leading === false ? 0 : _.now();
+            timeout = null;
+            result = func.apply(context, args);
+            if (!timeout) context = args = null;
+        };
+        return function () {
+            var now = _.now();
+            if (!previous && options.leading === false) previous = now;
+            var remaining = wait - (now - previous);
+            context = this;
+            args = arguments;
+            if (remaining <= 0 || remaining > wait) {
+                if (timeout) {
+                    clearTimeout(timeout);
+                    timeout = null;
+                }
+                previous = now;
+                result = func.apply(context, args);
+                if (!timeout) context = args = null;
+            } else if (!timeout && options.trailing !== false) {
+                timeout = setTimeout(later, remaining);
+            }
+            return result;
+        };
+    };
 
 
 
